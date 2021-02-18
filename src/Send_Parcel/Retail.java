3d4db1a73dcdc2a18183a1407ecae3fe27e10961
ImtiazAkash <imtia@ImtiazAkash.com>
2021-02-18 20:28:19 +0600

    master

--- src/Send_Parcel/Retail.java
+++ src/Send_Parcel/Retail.java
@@ -1,10 +1,5 @@
 package Send_Parcel;
 
 public class Retail {
-    public static void main(String[] args) {
-        System.out.println("whats up nigga");
-        System.out.println("eida notun try");
-        System.out.println("dehi to sync hoy kina");
-        System.out.println("eida abar lekhlam");
-    }
+   
 }
