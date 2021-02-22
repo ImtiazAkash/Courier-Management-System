8bac25c59f083081ff31fe69b859b9daa134ac59
ImtiazAkash <imtia@ImtiazAkash.com>
2021-02-22 20:49:08 +0600

    master

--- src/Send_Parcel/SendPackage_Controller.java
+++ src/Send_Parcel/SendPackage_Controller.java
@@ -56,16 +56,16 @@ public class SendPackage_Controller {
     private Label price_lbl;
 
     @FXML
+    private TextField price_txt;
+
+    @FXML
     private Label token_lbl;
 
     @FXML
+    private TextField token_txt;
+
+    @FXML
     private Button Book_btn;
-
-    @FXML
-    private Label showprice_lbl;
-
-    @FXML
-    private Label showtoken_lbl;
 
     Connection con;
     PreparedStatement pst;
@@ -79,54 +79,83 @@ public class SendPackage_Controller {
         String combinedChars = capitalCaseLetters + lowerCaseLetters + specialCharacters + numbers;
         Random random = new Random();
         char[] password = new char[12];
-  
+
         password[0] = lowerCaseLetters.charAt(random.nextInt(lowerCaseLetters.length()));
         password[1] = capitalCaseLetters.charAt(random.nextInt(capitalCaseLetters.length()));
         password[2] = specialCharacters.charAt(random.nextInt(specialCharacters.length()));
         password[3] = numbers.charAt(random.nextInt(numbers.length()));
-     
-        for(int i = 4; i< 12 ; i++) {
-           password[i] = combinedChars.charAt(random.nextInt(combinedChars.length()));
+
+        for (int i = 4; i < 12; i++) {
+            password[i] = combinedChars.charAt(random.nextInt(combinedChars.length()));
         }
         return password;
     }
 
+    public static String price(double weight) {
+        double price = weight * 60;
+        return price + " Taka";
+    }
+
     @FXML
-	public void next(ActionEvent event) throws IOException {
+    public void Show(ActionEvent event) throws IOException {
 
         String pickup = pickup_txt.getText();
         String delivery = delivery_txt.getText();
         String weight = package_weight_txt.getText();
+        double product_weight = Double.parseDouble(weight);
         char[] token = passgen();
         String token_id = String.valueOf(token);
+        String cost = price(product_weight);
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost/courier_db", "root", "");
-
-            pst = con.prepareStatement("insert into users_info(Pickup_point, Destination_point, Weight, token_no)values(?,?,?,?)");
-            pst.setString(1, pickup);
-            pst.setString(2, delivery);
-            pst.setString(3, weight);
-            pst.setString(4, token_id);
-
-            int status = pst.executeUpdate();
 
             if (pickup.equals("") || delivery.equals("") || weight.equals("")) {
                 JOptionPane.showMessageDialog(null, "TextField Cannot Be Empty");
-            } else if(status == 1){
-                //JOptionPane.showMessageDialog(null, "Record Added");
-                AnchorPane pane = FXMLLoader.load(getClass().getResource("/Send_Parcel/Book.fxml"));
-                sendpackage_anchor.getChildren().setAll(pane);
+            } else {
+                Class.forName("com.mysql.jdbc.Driver");
+                con = DriverManager.getConnection("jdbc:mysql://localhost/courier_db", "root", "");
+
+                pst = con.prepareStatement(
+                        "insert into users_info(Pickup_point, Destination_point, Weight, token_no, cost)values(?,?,?,?,?)");
+                pst.setString(1, pickup);
+                pst.setString(2, delivery);
+                pst.setString(3, weight);
+                pst.setString(4, token_id);
+                pst.setString(5, cost);
+
+                int status = pst.executeUpdate();
+                if (status == 1) {
+                    pst = con.prepareStatement("select cost, token_no from users_info where token_no=?");
+                    pst.setString(1, token_id);
+
+                    rs = pst.executeQuery();
+
+                    if(rs.next()) {
+                        String price = rs.getString("cost");
+                        price_txt.setText(price);
+                        String dbtoken = rs.getString("token_no");
+                        token_txt.setText(dbtoken);
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "Something Wrong! Try Again");
+                    }
+                    
+                } else {
+                    JOptionPane.showMessageDialog(null, "Something Wrong! Try Again");
+                }
             }
 
         } catch (ClassNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-            } catch (SQLException e) {
+        } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-            }
-    
+        }
+
+    }
+    @FXML
+    public void Book(ActionEvent event) {
+        JOptionPane.showMessageDialog(null, "Booking Success! Thank you for using our service");
     }
 }
